b4671ba1d84a212bb6acc55fd15ae82a92777f91
Pamilo <81716232+Pamilo@users.noreply.github.com>
2022-02-12 13:14:22 -0600

    Funciona con el mapa grande

--- laboratorios/lab01/codigo/Map.py
+++ laboratorios/lab01/codigo/Map.py
@@ -23,9 +23,9 @@ class Grapho:
                 print("  "+self.diccionarioNombre[vertice[0]]+" y esta a "+vertice[1]+" Km")
             print("-------------------------------------------------------------------------------")
 
+#archivos = open("laboratorios\lab01\codigo\mapa.txt",'r')
 
-
-archivos = open("laboratorios\lab01\codigo\mapa.txt",'r')
+archivos = open("laboratorios\lab01\codigo\medellin_colombia-grande.txt",'r')
 aristas=deque()
 vertices=deque()
 a = archivos.readline()
@@ -33,7 +33,12 @@ a = archivos.readline()
 if a == "\n":
     a=archivos.readline()
 while a.strip() != "":
-    vertices.append(a.split())
+    if len(a.split())== 4:
+        añadir=a.split()
+    else:
+        añadir=a.split()
+        añadir.append("Nombre Desconocido")
+    vertices.append(añadir)
     a = archivos.readline()
 
 a = archivos.readline()
--- laboratorios/lab01/codigo/test.py
+++ laboratorios/lab01/codigo/test.py
@@ -1,16 +1,19 @@
+from collections import deque
 archivos = open("laboratorios\lab01\codigo\mapa.txt",'r')
+archivos = open("laboratorios\lab01\codigo\medellin_colombia-grande.txt",'r')
+aristas=deque()
+vertices=deque()
 a = archivos.readline()
 a = archivos.readline()
-a = archivos.readline()
+if a == "\n":
+    a=archivos.readline()
 while a.strip() != "":
-    print(a.split())
+    if len(a.split())== 4:
+        añadir=a.split()
+    else:
+        añadir=a.split()
+        añadir.append("Nombre Desconocido")
+    vertices.append(añadir)
     a = archivos.readline()
-
-a = archivos.readline()
-a = archivos.readline()
-a = archivos.readline()
-while  a.strip() != "":
-        print(a.split())
-        a = archivos.readline()
-
+print (vertices)
 
