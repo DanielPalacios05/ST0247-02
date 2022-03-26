f8e890464cfcff0c25a6735afacb24b3399f39f1
Pamilo <81716232+Pamilo@users.noreply.github.com>
2022-03-25 20:57:45 -0600

    yo ya no se

--- proyecto/codigo/a.py
+++ proyecto/codigo/a.py
@@ -5,45 +5,49 @@ from collections import deque
 from math import inf
 
 
-def caminoCorto(grafo: GraphAL,n):
-    
-    visitados = [False] * grafo.size
-    
-    return caminoCortoAux(grafo, 0,n,visitados)
+def caminoCorto(grafo: GraphAL,begin, fin , riesgoMaximo):
+    return caminoCortoAux(grafo, begin, fin, riesgoMaximo)
 
-def caminoCortoAux(grafo:GraphAL,begin, fin,visitados):
-    visitados[begin]=True
-    if begin==fin:
-        rutaFinal=deque()
-        rutaFinal.appendleft(fin)
-        return (rutaFinal,0)
-    else:
-        distanciaMenor=inf
-        rutaCorta=deque()
-        for lugar in grafo.getSuccessors(begin):
-            
-            if not visitados[lugar]:
-                par=caminoCortoAux(grafo,int(lugar),fin,visitados)
-                rutaPosible=par[0]
-                distancia=par[1]
-                rutaPosible.appendleft(begin)
-                distanciaActual=grafo.getWeight(begin,lugar)+distancia
-                if distanciaActual<=distanciaMenor:
-                    rutaCorta=rutaPosible
-                    distanciaMenor=distanciaActual
-                visitados[lugar]=False
-        return rutaCorta,distanciaMenor
-    
-    
-    
-    
+def caminoCortoAux(grafo:GraphAL,begin, fin,riesgoMaximo):
+    n=grafo.size
+    visitados = [False] * n
+    distancias = [inf] * n
+    riesgos=[0]*n
+    posiciones = [-1] * n
+    distancias[begin] = 0
 
-data = pd.read_csv("proyecto\codigo\calles_de_medellin_con_acoso.csv",
-                   sep=";", usecols=[0, 1, 2, 3, 4, 5])
+    for i in range(n):
+        visitando = -1
+        for j in range(n):
+            if (not visitados[j]) and (visitando==-1 or distancias[j]<distancias[visitando]):
+                visitando=j
+        if distancias[visitando]==inf:
+            break
+        visitados[visitando]=True
+        
+        for vecino in grafo.getSuccessors(visitando):
+            metros=grafo.getWeight(visitando,vecino)
+            riesgo=grafo.getRisk(visitando,vecino)
+            if riesgos[visitando]+riesgo<riesgoMaximo:
+                if distancias[visitando]+metros<distancias[vecino]:
+                    distancias[vecino]=distancias[visitando]+metros
+                    riesgos[vecino]=riesgos[visitando]+riesgo
+                    posiciones[vecino] = visitando
+    
+    caminar=distancias[fin]
+    asalto=riesgos[fin]
+    camino=deque()
+    actual=fin
+    while actual!=begin:
+        camino.appendleft(actual)
+        actual=posiciones[actual]
+    return camino, caminar, asalto
+
+data = pd.read_csv("proyecto\codigo\calles_de_medellin_con_acoso.csv",sep=";", usecols=[0, 1, 2, 3, 4, 5])
 
 arrdata = data.to_numpy()
 
-print(arrdata)
+#print(arrdata)
 
 
 mapa = GraphAL()
@@ -61,7 +65,7 @@ for i in arrdata:
         mapa.vertices[i[2]] = numberofNodes
         numberofNodes = numberofNodes+1
         
-print(numberofNodes)
+#print(numberofNodes)
 
 mapa.iniList(numberofNodes)
 
@@ -71,8 +75,8 @@ for i in arrdata:
     if i[4]:
         mapa.addUndirArc(mapa.vertices[i[1]],mapa.vertices[i[2]],(i[0],i[3],i[5]))
     else:
-         mapa.addArc(mapa.vertices[i[1]],mapa.vertices[i[2]],(i[0],i[3],i[5]))
-         
-print(caminoCorto(mapa,1))
+        mapa.addArc(mapa.vertices[i[1]],mapa.vertices[i[2]],(i[0],i[3],i[5]))
+        
+print(caminoCorto(mapa,0,6,2))
     
     
