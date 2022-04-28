a8509c702ac898942878e89c4ec468b07f5112cf
Pamilo <81716232+Pamilo@users.noreply.github.com>
2022-04-28 10:07:15 -0500

    Update a (1).py

--- proyecto/codigo/a (1).py
+++ proyecto/codigo/a (1).py
@@ -45,6 +45,46 @@ def caminoCortoAux(grafo:GraphAL,begin, fin,riesgoMaximo):
             break
     return camino, caminar, asalto
 
+def caminoSeguro(grafo: GraphAL,begin, fin , distanciaMaximo):
+    return caminoCortoAux(grafo, begin, fin, distanciaMaximo)
+
+def caminoSeguroAux(grafo:GraphAL,begin, fin,distanciaMaximo):
+    n=grafo.size
+    visitados = [False] * n
+    distancias = [0] * n
+    riesgos=[inf]*n
+    posiciones = [-1] * n
+    distancias[begin] = 0
+
+    for i in range(n):
+        visitando = -1
+        for j in range(n):
+            if (not visitados[j]) and (visitando==-1 or riesgos[j]<riesgos[visitando]):
+                visitando=j
+        if distancias[visitando]==inf:
+            break
+        visitados[visitando]=True
+        for vecino in grafo.getSuccessors(visitando):
+            metros=grafo.getWeight(visitando,vecino)
+            riesgo=grafo.getRisk(visitando,vecino)
+            if distancias[visitando]+metros<distanciaMaximo:
+                if riesgos[visitando]+riesgo<riesgos[vecino]:
+                    distancias[vecino]=riesgos[visitando]+metros
+                    riesgos[vecino]=riesgos[visitando]+riesgo
+                    posiciones[vecino] = visitando
+    
+    caminar=distancias[fin]
+    asalto=riesgos[fin]
+    camino=deque()
+    actual=fin
+    while actual!=begin:
+        camino.appendleft(actual)
+        actual=posiciones[actual]
+        if (actual==-1):
+            camino = deque()
+            break
+    return camino, caminar, asalto
+
 #data = pd.read_csv("calles_de_medellin_con_acoso.csv",sep=";", usecols=[0, 1, 2, 3, 4, 5])
 
 #arrdata = data.to_numpy()
@@ -72,6 +112,7 @@ for i in range(len(arrdata)):
         mapa.addUndirArc(mapa.vertices[arrdata["origin"][i]],mapa.vertices[arrdata["destination"][i]],(arrdata["name"][i],arrdata["length"][i],arrdata["harassmentRisk"][i]))
     else:
         mapa.addArc(mapa.vertices[arrdata["origin"][i]],mapa.vertices[arrdata["destination"][i]],(arrdata["name"][i],arrdata["length"][i],arrdata["harassmentRisk"][i]))
-print(caminoCorto(mapa,0,40,1000))
+print(caminoCorto(mapa,0,40,5))
+print(caminoSeguro(mapa,0,40,500))
     
     
